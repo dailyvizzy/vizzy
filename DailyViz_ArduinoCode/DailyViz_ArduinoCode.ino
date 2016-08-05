841d76f3fbd4a5a7166ce36f58691a8e1659375e
Andrew Kleindolph <akleindolph@gmail.com>
2016-08-04 17:12:24 -0700

    arduino code complete

--- DailyViz_ArduinoCode/DailyViz_ArduinoCode.ino
+++ DailyViz_ArduinoCode/DailyViz_ArduinoCode.ino
@@ -11,6 +11,12 @@ const int four = 10;
 const int five = 9;
 const int reset = 8;
 
+const int LEDone = 7;
+const int LEDtwo = 6;
+const int LEDthree = 5;
+const int LEDfour = 4;
+const int LEDfive = 3;
+
 int buttonDelay = 50;
 
 void setup() { // initialize the buttons' inputs:
@@ -21,37 +27,49 @@ void setup() { // initialize the buttons' inputs:
   pinMode(five, INPUT);
   pinMode(reset, INPUT);
 
+  pinMode(LEDone, OUTPUT);
+  pinMode(LEDtwo, OUTPUT);
+  pinMode(LEDthree, OUTPUT);
+  pinMode(LEDfour, OUTPUT);
+  pinMode(LEDfive, OUTPUT);
+
   Serial.begin(9600);
   Keyboard.begin();
+
 }
 
 void loop() {
   // use the pushbuttons to control the keyboard:
   if (digitalRead(one) == HIGH) {
+    digitalWrite(LEDone, HIGH);
     delay(buttonDelay);
     if (digitalRead(one) == LOW) {
       Keyboard.write('1');
     }
   }
   else if (digitalRead(two) == HIGH) {
+    digitalWrite(LEDtwo, HIGH);
     delay(buttonDelay);
     if (digitalRead(two) == LOW) {
       Keyboard.write('2');
     }
   }
   else if (digitalRead(three) == HIGH) {
+    digitalWrite(LEDthree, HIGH);
     delay(buttonDelay);
     if (digitalRead(three) == LOW) {
       Keyboard.write('3');
     }
   }
   else if (digitalRead(four) == HIGH) {
+    digitalWrite(LEDfour, HIGH);
     delay(buttonDelay);
     if (digitalRead(four) == LOW) {
       Keyboard.write('4');
     }
   }
   else if (digitalRead(five) == HIGH) {
+    digitalWrite(LEDfive, HIGH);
     delay(buttonDelay);
     if (digitalRead(five) == LOW) {
       Keyboard.write('5');
@@ -63,4 +81,9 @@ void loop() {
       Keyboard.write('0');
     }
   }
+  digitalWrite(LEDone, LOW);
+  digitalWrite(LEDtwo, LOW);
+  digitalWrite(LEDthree, LOW);
+  digitalWrite(LEDfour, LOW);
+  digitalWrite(LEDfive, LOW);
 }
